6477f9179c9744eea37ad0939b8ad9f0a41acbd8
John Griffiths <gnuchu@gmail.com>
2022-10-27 03:48:15 +0100

    actions etc

---
+++ isup_test.py
@@ -0,0 +1,17 @@
+import unittest
+import subprocess
+import re
+
+HOSTNAME="68.183.37.68"
+PORT="22"
+
+class TestSum(unittest.TestCase):
+
+    def test_succcess(self):
+        result = subprocess.run(["./isup", f"{HOSTNAME}", f"{PORT}"], capture_output=True)
+        command_succceed = ( str(result.stderr).find("succeeded") > 0 )
+
+        self.assertEqual(command_succceed, True)
+
+if __name__ == '__main__':
+    unittest.main()
